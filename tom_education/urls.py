4df9516923cbdaee3ae453e001c66b3656aaa088
Joe Singleton <joesingo@gmail.com>
2019-06-26 11:50:06 +0100

    Fix target view URLs to avoid clobbering create/import/etc from tom_targets

--- tom_education/urls.py
+++ tom_education/urls.py
@@ -6,5 +6,5 @@ app_name = "tom_education"
 
 urlpatterns = [
     path('observations/<str:facility>/create/', TemplatedObservationCreateView.as_view(), name='create_obs'),
-    path('targets/<pk>/', TimelapseTargetDetailView.as_view(), name='target_detail'),
+    path('targets/<int:pk>/', TimelapseTargetDetailView.as_view(), name='target_detail'),
 ]
