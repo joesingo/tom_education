353eeb0ce1ff9d911380e94335526e3f585aa29d
Joe Singleton <joesingo@gmail.com>
2019-06-28 15:16:51 +0100

    Create timelapses synchronously if django_dramatiq is not in use

--- tom_education/tasks.py
+++ tom_education/tasks.py
@@ -1,9 +1,20 @@
-import dramatiq
+from django.conf import settings
 
 from tom_education.models import TimelapseDataProduct
 
+def task(func, **kwargs):
+    """
+    Decorator that wraps dramatiq.actor, but does not import dramatiq if
+    django_dramatiq is not in use
+    """
+    if 'django_dramatiq' in settings.INSTALLED_APPS:
+        import dramatiq
+        return dramatiq.actor(func, **kwargs)
 
-@dramatiq.actor
+    func.send = func
+    return func
+
+@task
 def make_timelapse(tl_prod_pk):
     """
     Task to create the timelapse for the given TimelapseDataProduct
