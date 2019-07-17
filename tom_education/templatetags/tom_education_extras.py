c37c7d51261361299b8648deb2bb6dd91ab0c58c
Joe Singleton <joesingo@gmail.com>
2019-07-17 16:11:58 +0100

    Fix 'is_reduced' logic for data product checkboxes

--- tom_education/templatetags/tom_education_extras.py
+++ tom_education/templatetags/tom_education_extras.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 @register.inclusion_tag('tom_education/dataproduct_checkbox.html')
 def dataproduct_checkbox(product):
-    is_reduced = 'e91.fits.fz' not in product.data.name
+    is_reduced = not product.data.name.endswith('e00.fits.fz')
     return {'product': product, 'reduced': is_reduced}
 
 @register.simple_tag
