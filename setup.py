5fac0f444f3eaad1204635e25c16361844513ce6
Joe Singleton <joesingo@gmail.com>
2019-08-06 15:06:05 +0100

    Add Travis configuration

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Joe Singleton',
     author_email='joesingo@gmail.com',
     install_requires=[
-        'tomtoolkit',
+        'tomtoolkit[test]==0.5.1',  # TODO: install test deps in extras_require
         'numpy',
         'imageio-ffmpeg==0.3.0',
         'imageio==2.5.0',
