67d0f2e22e86bf6e1cdab94e5de6f725aa818489
Edward Gomez <edward@gomez.me.uk>
2019-09-30 16:12:38 +0100

    Uprev

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='tom_education',
-    version='1.0.0',
+    version='1.1.0',
     description='TOM toolkit plugin for educational projects',
     long_description=long_description,
     long_description_content_type='text/markdown',
